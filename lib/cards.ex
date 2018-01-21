d4f1c424ddfb7e92ee5bb4c07e8d3e192125002c
Victor Peres <victor.peres93@gmail.com>
2018-01-21 17:52:11 -0200

    Cards module shuffling, dealing

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,23 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
-  @doc """
-  Hello world.
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  ## Examples
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-      iex> Cards.hello
-      :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-  """
-  def hello do
-    :world
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
